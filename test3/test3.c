846263e062c993f1453f90290cdd76ada8df65c0
unknown <md02.06ismail2021@gmail.com>
2022-08-01 21:07:53 +0600

    Test3

--- test3/test3.c
+++ test3/test3.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    printf("Usage: %s [options]\n", argv[0]);
+    int q,w,e,r,t;
+    printf("Enter: ");
+    scanf("%d", &r);
+    e=1;
+    r=r;
+    while (e<=r) {
+        printf("Enter number of digits: ");
+        scanf("%d", &q);
+
+        printf("Enter number of digits: ");
+        scanf("%d", &w);
+
+        printf("%d + %d = %d\n", q, w, q+w);
+        printf("%d + %d = %d\n", q, w, q-w);
+        printf("%d + %d = %d\n", q, w, q*w);
+        printf("%d + %d = %d\n", q, w, q/w);
+        e++;
+}
+    return 0;
 }
