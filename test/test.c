278eb80260815f81198f3efb6d117c45d2099b91
Ismail_hossain <md02.06ismail2021@gmail.com>
2022-08-03 21:03:42 +0600

    add

---
+++ test/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    char name[100];
+    int i;
+    scanf("%[^\n]s", name);
+    for (i = 1; i <= 10; i++) {
+
+        printf("%d: %s\n", i, name);
+    }
+    return 0;
+}
