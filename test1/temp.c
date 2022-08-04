cdb687ae58c42319b883bb5010cfb2f979a818ad
Ismail_hossain <md02.06ismail2021@gmail.com>
2022-08-04 21:10:50 +0600

    add

--- test1/temp.c
+++ test1/temp.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-float temp(int x);
-float temp2(int x);
-float temp3(int x);
-float temp4(int x);
-float temp5(int x);
-float temp6(int x);
+float tempCF(int x);
+float tempCK(int x);
+
+float tempFC(int x);
+float tempFK(int x);
+
+float tempKC(int x);
+float tempKF(int x);
 
 
 
@@ -17,11 +19,16 @@ int main(int argc, char *argv[]) {
     printf("Enter number: ");
     scanf("%f", &n);
     if (c == 'c') {
-        printf("Fahrenheit: %0.3f\n", temp(n));
-        printf("Kelvin: %0.3f", temp2(n));
+        printf("Fahrenheit: %0.3f\n", tempCF(n));
+        printf("Kelvin: %0.3f", tempCK(n));
     }
     else if (c == 'f') {
-
+        printf("Celsius: %0.3f\n", tempFC(n));
+        printf("Kelvin: %0.3f", tempFK(n));
+    }
+    else if (c == 'k') {
+        printf("Celsius: %0.3f\n", tempKC(n));
+        printf("Fahrenheit: %0.3f", tempKF(n));
     }
     
     return 0;
@@ -29,30 +36,44 @@ int main(int argc, char *argv[]) {
 
 //Celsius to Fahrenheit
 
-float temp(int x) {
-    float tem= x * 1.8 + 32;
+float tempCF(int x) {
+    float tem= x * 9/5 + 32;
     return tem;
 }
 
 //Celsius to Kelvin
 
-float temp2(int x) {
+float tempCK(int x) {
     float tem= x + 273.15;
     return tem;    
 }
 
+
 //Fahrenheit to Celsius
 
-float temp2(int x) {
-    float tem= (x - 32) * 1.8;
+float tempFC(int x) {
+    float tem= (x - 32) * 5/9;
     return tem;    
 }
 
 //Fahrenheit to Kelvin
 
-float temp2(int x) {
-    float tem= (x - 32) * 1.8 + 273.15;
+float tempFK(int x) {
+    float tem= (x - 32) * 5/9 + 273.15;
     return tem;    
 }
 
+
 //kelvin to celsius
+
+float tempKC(int x) {
+    float tem= x - 273.15;
+    return tem;    
+}
+
+//kelvin to Fahrenheit
+
+float tempKF(int x) {
+    float tem= (x - 273.15) * 9/5 + 32;
+    return tem;    
+}
