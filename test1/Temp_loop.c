ed2a0ba70fc8b97b1f8523674d0337e2209edacc
Ismail_hossain <md02.06ismail2021@gmail.com>
2022-08-07 21:37:53 +0600

    add

---
+++ test1/Temp_loop.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main (int argc, char *argv[]) {
+    float t;
+    char c;
+    printf("Enter: ");
+    scanf("%f", &t);
+    printf("Enter temperature: ");
+    scanf("%c", &c);
+    if (c == 'c') {
+
+    }
+    else if (c == 'f') {
+
+    }
+    else if (c == 'k') {
+
+    }
+    return 0;
+}
