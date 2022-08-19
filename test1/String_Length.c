f03993a91996b5b71250b512ce1b78036d52cb52
Ismail_hossain <md02.06ismail2021@gmail.com>
2022-08-19 20:19:03 +0600

    Add

--- test1/String_Length.c
+++ test1/String_Length.c
@@ -1,34 +1,13 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    int n;
-    char ii[4][10]={"Ismail","Rayhan","Yeasin","Azmain"};
-    printf("Enter a Number: ");
-    scanf("%d",&n);
+    int a,b=10;
+    printf("Enter a number: ");
+    scanf("%d",&b);
 
-    switch (n)
-    {
-    case 1:
-        printf(ii[0]);
-        break;
-
-    case 2:
-        printf(ii[1]);
-        break;
-
-    case 3:
-        printf(ii[2]);
-        break;
-
-    case 4:
-        printf(ii[3]);
-        break;
-
-    default:
-        printf("Invalid number\n");
-        break;
+    for (int a=0; a<b; a++) {
+        printf("Your number b is: %d\n", a+=1);
     }
-
 
     return 0;
 }
---
+++ test1/tempCodeRunnerFile.c
@@ -0,0 +1,3 @@
+  // for (int a=0; a<b; a++) {
+    //     printf("Your number b is: %d\n", a++);
+    // }
